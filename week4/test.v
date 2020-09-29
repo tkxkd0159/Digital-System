03679b6ae961196152169bb89c468e9c353af358
RLholic <tkxkd0159@gmail.com>
2020-09-29 10:48:58 +0900

    The wave test of array type input and output

--- week4/test.v
+++ week4/test.v
@@ -1,12 +1,31 @@
-module test(
-	input inp1, inp2, inp3, inp4,
-	output result
+	module test(
+	input [3:0] A,
+	output S3,
+	output S2,
+	output S1,
+	output S0
 );
 
 
-	assign t1 = inp1 & inp2;
-	assign t2 = inp3 & inp4;
-	assign result = ~(t1|t2);
-	
+reg [3:0] out;
+
+always @(A)
+begin
+case({A})
+4'b0000 : out <= 4'b0000; //0
+4'b0001 : out <= 4'b0001; //1
+4'b0010 : out <= 4'b0010; //2
+4'b0011 : out <= 4'b0011; //3
+4'b0100 : out <= 4'b0100; //4
+4'b0101 : out <= 4'b0101; //5
+4'b0110 : out <= 4'b0110; //6
+4'b0111 : out <= 4'b0111; //7
+4'b1000 : out <= 4'b1000; //8
+4'b1001 : out <= 4'b1001; //9
+default : out <= 4'b0000; //NULL
+endcase 
+end
+
+assign {S3,S2,S1,S0} = out;
+
 endmodule
-	
