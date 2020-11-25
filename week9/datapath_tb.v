a9739279d3390699f3f0596751a46311f6149126
LJS_KU <tkxkd0159@gmail.com>
2020-11-25 19:19:45 +0900

    fix FS array structure
    Fix structure to connect with the control unit

--- week9/datapath_tb.v
+++ week9/datapath_tb.v
@@ -3,7 +3,7 @@
 module datapath_tb;
 
 reg CLK;
-reg [12:0] CW; // DA,AA,BA,muxB,{Cin, S, [1:0] S1}, muxD, write
+reg [12:0] CW; // DA,AA,BA,muxB,{S, [1:0] S1, Cin}, muxD, write
 reg [3:0] CONSTANT;
 reg [3:0] DATA;
 wire [3:0] reg0, reg1, reg2, reg3;
@@ -13,13 +13,13 @@ datapath RES0(CLK, CW, CONSTANT, DATA, reg0, reg1, reg2, reg3);
 initial
  begin
      CW<=13'b0000000000000; CONSTANT<=4'b0000; DATA<=4'b0000;
-     #10 DATA<=4'b0101; CW<=13'b0000000000011; //reg0
+     #10 DATA<=4'b0100; CW<=13'b0000000000011; //reg0
      #10 DATA<=4'b0101; CW<=13'b0100000000011; //reg1
      #10 DATA<=4'b0110; CW<=13'b1000000000011; //reg2
      #10 DATA<=4'b0111; CW<=13'b1100000000011; //reg3
 
-     #20 CW<=13'b1100010000101;  // R3 = R0+R1 -> 9
-     #20 CW<=13'b1011010101001;DATA<=4'b0011;  // R2 = R3-R1 -> 4
+     #20 CW<=13'b1100010001001;  // R3 = R0+R1 -> 9
+     #20 CW<=13'b1011010010101;DATA<=4'b0011;  // R2 = R3-R1 -> 4
 
  end
 
--- week9/functionUnit.v
+++ week9/functionUnit.v
@@ -1,6 +1,6 @@
 module functionUnit(
     input [3:0] Adata, Bdata,
-    input [3:0] FS,   //  {Cin, S, S1}
+    input [3:0] FS,   //  {S, [1:0]S1, Cin}
     output [3:0] Fout
 );
 
@@ -10,9 +10,9 @@ wire [3:0] ArithOut;
 wire [3:0] Cout;
 wire [3:0] LogicOut;
 
-arithmeticCircuit CIRCUIT1(Adata, Bdata, FS[1:0],FS[3], ArithOut, Cout);
-logicCircuit CIRCUIT2(Adata, Bdata, FS[1:0], LogicOut);
+arithmeticCircuit CIRCUIT1(Adata, Bdata, FS[2:1],FS[0], ArithOut, Cout);
+logicCircuit CIRCUIT2(Adata, Bdata, FS[2:1], LogicOut);
 
-MUX2to1 ALUMux(LogicOut, ArithOut, FS[2], Fout);
+MUX2to1 ALUMux(LogicOut, ArithOut, FS[3], Fout);
 
 endmodule
